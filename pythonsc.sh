e5191232fb4288e5024fcaa606018e3715e8cbfc
Kraken0306 <gaurav030695@gmail.com>
2020-06-08 23:00:29 -0700

    first commit

---
+++ pythonsc.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import pandas as pd
+import numpy as np
+df=pd.read_csv(‘flightdelay2007.csv’)
+df
+#Question1
+first3sfo=df[df[‘Origin’]==’SFO’][[‘Origin’,’ArrDelay’]]
+print(first3sfo.head(3))
+#Question2
+print(df[‘Dest’].value.counts().head(4))
